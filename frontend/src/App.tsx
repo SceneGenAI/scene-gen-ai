777ea5da4e89f30cb4a069195e7c94a2df1fd155
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-07 17:03:26 +0300

    Formating files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import React from 'react'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home/Home';
-import Generate from './pages/Generate/Generate';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home/Home'
+import Generate from './pages/Generate/Generate'
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/generate" element={<Generate />} />
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App

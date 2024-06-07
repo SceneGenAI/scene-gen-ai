3e74ee86b1dcc1ba9923b0a7a90cf094975c00cc
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-07 18:57:09 +0300

    Change styles Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,29 +3,6 @@ import './Header.css'
 import { Link } from 'react-router-dom'
 import BurgerMenu from '../BurgerMenu/BurgerMenu'
 
-// const pages = [
-//   {
-//     name: 'SceneGenAI',
-//     link: '/'
-//   },
-//   {
-//     name: 'Sign In',
-//     link: '/signin'
-//   },
-//   {
-//     name: 'Sign Up',
-//     link: '/signUp'
-//   },
-//   {
-//     name: 'History',
-//     link: '/history'
-//   },
-//   {
-//     name: 'profile',
-//     link: '/profile'
-//   }
-// ]
-
 const Header: React.FC = () => {
   return (
     <header className="header">
@@ -36,9 +13,7 @@ const Header: React.FC = () => {
         </div>
         <div className="header-section header-center">
           <div className="header-item">
-            <p>
-              <Link to="/generate">Ru</Link>
-            </p>
+            <Link to="/generate" className='underlined'>Ru</Link>
           </div>
           <div className="header-item">
             <img src="./src/components/icons/temp.svg" alt="moon-icon" className="moon-icon" />
@@ -46,12 +21,12 @@ const Header: React.FC = () => {
         </div>
         <div className="header-section header-right">
           <div className="header-item">
-            <p>Sign In</p>
+            <Link to="/sign-in" className='underlined'>Sign In</Link>
           </div>
           <div className="header-item">
-            <div className="sign-in-button">
+            <Link to="/sign-up" className='sign-up-button'>
               <p>Sign Up</p>
-            </div>
+            </Link>
           </div>
         </div>
       </div>

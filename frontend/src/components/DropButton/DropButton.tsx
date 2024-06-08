716540cb64e54f8dcc62d2e4ab886a04ec4dada0
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-08 18:04:31 +0300

    Added DropButton for loading images

---
+++ frontend/src/components/DropButton/DropButton.tsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState, useEffect } from 'react';
+import './DropButton.css';
+
+interface DropButtonProps {
+  onImageUpload: (files: FileList) => void;
+}
+
+const DropButton: React.FC<DropButtonProps> = ({ onImageUpload }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleDrop = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(false);
+    const files = event.dataTransfer?.files;
+    if (files && files.length) {
+      onImageUpload(files); // Pass FileList object
+    }
+  };
+
+  const handleDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(false);
+  };
+
+  const handleFilesUpload = (files: FileList) => {
+    onImageUpload(files); // Pass FileList object directly
+  };
+
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length) {
+      onImageUpload(files); // Pass FileList object
+    }
+  };
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('drop', handleDrop);
+    document.addEventListener('dragover', handleDragOver);
+    document.addEventListener('dragleave', handleDragLeave);
+    return () => {
+      document.removeEventListener('drop', handleDrop);
+      document.removeEventListener('dragover', handleDragOver);
+      document.removeEventListener('dragleave', handleDragLeave);
+    };
+  }, []);
+
+  return (
+    <div className="drop-button-container" onClick={handleClick}>
+      <p>Выберите файл</p>
+      <input
+        type="file"
+        id="file"
+        multiple
+        ref={fileInputRef}
+        onChange={handleFileInputChange}
+        style={{ display: 'none' }}
+        title="Выберите файл для загрузки"
+      />
+      {isDragging && <div className="overlay" />}
+    </div>
+  );
+};
+
+export default DropButton;

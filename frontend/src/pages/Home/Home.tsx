902d942244f8d1de58d700dac81c1184222083c6
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-08 12:31:24 +0300

    Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import './Home.css'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home page!</p>
+    <div className='home'>
+      <div className="text-container">
+        <p className='text-container-header'>Онлайн генератор сцен</p>
+        <p className='text-container-description'>Сгенерируйте сцену для своего товара</p>
+      </div>
+      <div className="button-container">
+        <Link to="/generate" className='begin-button'>
+          <p>Начать</p>
+        </Link>
+      </div>
+      <div className="images-container">
+        <img src="./src/assets/swing-before.png" alt="swing-before" className='img-show' />
+        <img src="./src/components/icons/Arrow.svg" alt="arrow" className='arrow' />
+        <img src="./src/assets/swing-after.png" alt="swing-after" className='img-show' />
+      </div>
     </div>
   )
 }

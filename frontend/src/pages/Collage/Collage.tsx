9f9b618f4fcc33c840f80330deab1cfb3746878d
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-08 21:26:49 +0300

    Collage page added

--- frontend/src/pages/Collage/Collage.tsx
+++ frontend/src/pages/Collage/Collage.tsx
@@ -1,12 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import './Collage.css'
 
-const About: React.FC = () => {
+interface DropdownOption {
+  value: string
+  label: string
+}
+
+const Collage: React.FC = () => {
+  const location = useLocation()
+  const { options } = location.state as {
+    options: { value1: DropdownOption; value2: DropdownOption }
+  }
+
+  const [images, setImages] = useState<string[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/get-images', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            option1: options.value1.value,
+            option2: options.value2.value,
+          }),
+        })
+
+        const result = await response.json()
+        setImages(result.images)
+      } catch (error) {
+        console.error('Error:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    sendRequest()
+  }, [options])
+
   return (
     <div>
-      <h1>Collage page</h1>
-      <p>Learn more about us on this page.</p>
+      {loading ? (
+        <div>Загрузка...</div>
+      ) : (
+        <div className="collage">
+          <div className="image-display-container-collage">
+            {images.map((image, index) => (
+              <div className="image-preview-container-collage" key={index}>
+                <a
+                  href={`data:image/png;base64,${btoa(image)}`}
+                  download={`collage_${index + 1}.png`}
+                >
+                  <img
+                    src={`data:image/png;base64,${btoa(image)}`}
+                    alt={`Collage ${index + 1}`}
+                    className="image-preview-collage"
+                  />
+                </a>
+                <img
+                  src="./src/components/icons/Download.svg"
+                  alt={`download ${index + 1}`}
+                  className="download-icon"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
 
-export default About
+export default Collage

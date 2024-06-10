b0718a8180dc069d9d5738093839485ff6bcf5d6
Nurislam Zinnatullin <n.zinnatullin@innopolis.university>
2024-06-10 22:21:13 +0300

    Loader added

---
+++ frontend/src/components/RingSpinner/RingSpinner.tsx
@@ -0,0 +1,12 @@
+import './RingSpinner.css'
+import React from 'react'
+
+const RingSpinner: React.FC = () => {
+  return (
+    <div className="ring-spinner-wrapper">
+      <div className="ring-spinner"></div>
+    </div>
+  );
+};
+
+export default RingSpinner;
---
+++ frontend/src/components/RippleSpinner/RippleSpinner.tsx
@@ -0,0 +1,13 @@
+import './RippleSpinner.css';
+import React from 'react';
+
+const RippleSpinner: React.FC = () => {
+  return (
+    <div className="ripple-spinner">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default RippleSpinner;

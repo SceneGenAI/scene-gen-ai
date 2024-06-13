75d2c6eb845c147f979b8efd91dcc2c95ecda947
Amir Nigmatullin <am.nigmatullin@innopolis.university>
2024-06-13 19:11:47 +0300

    updated IAMTOKEN

--- tr.py
+++ tr.py
@@ -1,16 +1,6 @@
 import requests
 
-# TODO  Заходим сюда "https://oauth.yandex.ru/verification_code#access_token=y0_AgAAAAAgVAurAATuwQAAAAEHONBcAAAKNSixRqxF-LytCIu05GNPviYm8Q&token_type=bearer&expires_in=31513279&cid=czrmkfkupwk8qbu20xjxxhhxuw"
-# TODO копируем код и открываем PowerShell
-
-'''
-$yandexPassportOauthToken = "<СЮДА ПИШЕМ КОД>"
-$Body = @{ yandexPassportOauthToken = "$yandexPassportOauthToken" } | ConvertTo-Json -Compress
-Invoke-RestMethod -Method 'POST' -Uri 'https://iam.api.cloud.yandex.net/iam/v1/tokens' -Body $Body -ContentType 'Application/json' | Select-Object -ExpandProperty iamToken
-'''
-# TODO получаем IAM_TOKEN и вставляем сюда
-
-IAM_TOKEN = ""
+IAM_TOKEN = "t1.9euelZqWm8eNmc2PyprHycqcnM6elO3rnpWal8-WkJqMj5iUm5eTl5qWmcvl8_cyZFNM-e82MXw6_N3z93ISUUz57zYxfDr8zef1656Vms6YmsuPnZKZjYyNzZ6Xx8zJ7_zF656Vms6YmsuPnZKZjYyNzZ6Xx8zJ.YzL5ylD3gorQQt98OCc-O37YqYfIa0b8EDni6K74PAMwEhu4A8vQagBbrPwaFFoE3_hofVt_JL6qnJocJakQCg"
 folder_id = 'b1g2k660gd43iko0496i'
 target_language = 'ru'
 texts = ["Hello", "World"]
